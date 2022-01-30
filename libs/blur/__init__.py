af6eeb142d6a7c9470bc6d8bc4522217ef00d58d
Vipul Aggarwal <vipulagg@microsoft.com>
2022-01-30 22:41:49 +0530

    dct blur

--- libs/blur/__init__.py
+++ libs/blur/__init__.py
@@ -1,1 +1,2 @@
 from libs.blur.blur_detection import get_blur_value
+from libs.blur.blur_detection import get_dct_blur_value
--- libs/blur/blur_detection.py
+++ libs/blur/blur_detection.py
@@ -1,8 +1,14 @@
 import cv2 as cv
 import numpy as np
-
+from . import dct_blur_detector
 
 def get_blur_value(image: np.array):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     fm = cv.Laplacian(gray, cv.CV_64F).var()
     return round(fm, 2)
+
+def get_dct_blur_value(image: np.array):
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    dctBlurDetector = dct_blur_detector.BlurDetector(downsampling_factor=4, num_scales=4, scale_start=2, num_iterations_RF_filter=3)
+    blur_map = dctBlurDetector.detectBlur(gray)
+    return np.mean(blur_map, axis=(0, 1))
