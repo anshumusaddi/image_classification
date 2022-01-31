2640e7c8d3359e3dda062b4a6a1a3b2f956e946c
Vipul Aggarwal <vipulagg@microsoft.com>
2022-02-01 00:26:40 +0530

    sharpess code

--- libs/sharpness/sharpness_detection.py
+++ libs/sharpness/sharpness_detection.py
@@ -1,8 +1,9 @@
-import cpbd
+import libs.cpbd as cpbd
 import numpy as np
+import cv2 as cv
 
 
 def get_sharpness_value(image: np.array):
-    sharpness = cpbd.compute(image)
-    print(sharpness)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    sharpness = cpbd.compute(gray)
     return sharpness
