59a774f03ff9b824829e159b8de6dbac2917655d
Vipul Aggarwal <vipulagg@microsoft.com>
2022-02-13 15:54:06 +0530

    Update face_calculation.py

--- libs/scoring/face_calculation.py
+++ libs/scoring/face_calculation.py
@@ -1,1 +1,18 @@
 from utils import calculate_min_max_of_list, min_max_normalize
+import numpy as np
+
+
+def get_group_emotion_score(faces):
+    
+    sad = 0
+    happy = 0
+    for face in faces:
+        sad = sad + (face["face_weight"] * face["face_feature"]["emotion"]["sad"])
+        happy = happy + (face["face_weight"] * face["face_feature"]["emotion"]["happy"])
+
+    return np.array([sad, happy])
+    
+
+def get_face_cosine_sim(face, groupEmotion):
+    emo = np.array([face["face_feature"]["emotion"]["sad"]), face["face_feature"]["emotion"]["happy"])])
+    return np.dot(emo, groupEmotion)/(np.linalg.norm(emo)*np.linalg.norm(groupEmotion))
