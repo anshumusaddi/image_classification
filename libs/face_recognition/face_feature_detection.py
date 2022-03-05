31576f0b1520fa15174fafaebf7be78fae77170c
Anshu Musaddi <anshu.musaddi@hotstar.com>
2022-03-05 20:52:03 +0530

    Anshu Musaddi | Added models for face feature
    Signed-off-by: Anshu Musaddi <anshu.musaddi@hotstar.com>

--- libs/face_recognition/face_feature_detection.py
+++ libs/face_recognition/face_feature_detection.py
@@ -3,7 +3,7 @@ from deepface import DeepFace
 
 def get_face_feature(image):
     try:
-        image_features = DeepFace.analyze(image, enforce_detection=False, prog_bar=False)
+        image_features = DeepFace.analyze(image, enforce_detection=False, prog_bar=False, actions=('emotion', 'race'))
     except:
         return None
     
